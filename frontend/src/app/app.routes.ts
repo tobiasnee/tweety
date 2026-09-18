a310341f5d7fc83f6478c0e81182d41576c8587c
Tobias Nee <tobias.nee@atos.net>
2026-09-18 08:40:39 +0200

    routing to user page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Hello } from './hello/hello';
+import { User } from './user/user';
 
 export const routes: Routes = [
-    { path: 'hello', component: Hello }
+    { path: 'hello', component: Hello },
+    { path: 'user', component: User }
 ];
