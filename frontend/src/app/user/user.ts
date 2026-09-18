d9300369566ca767f7431ad4306f07ed2bb9d44b
Tobias Nee <tobias.nee@atos.net>
2026-09-18 08:31:27 +0200

    new user component

---
+++ frontend/src/app/user/user.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-user',
+  styleUrl: './user.css',
+  templateUrl: './user.html',
+})
+export class User {}
