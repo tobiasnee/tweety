4cf5966c5293c7e9e9e0f643418b222bbc501a4f
Tobias Nee <tobias.nee@atos.net>
2026-09-18 16:36:16 +0200

    view profile

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Hello } from './hello/hello';
 import { User } from './user/user';
+import { Profile } from './profile/profile';
 
 export const routes: Routes = [
     { path: 'hello', component: Hello },
-    { path: 'user', component: User }
+    { path: 'user', component: User },
+    { path: 'users/:id', component: Profile }
 ];
